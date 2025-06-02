d37ff5faef2faf2d0868ca35c055ea425ea61683
Eiyprel <181449046+aprliwln@users.noreply.github.com>
2025-06-03 00:45:20 +0700

    Circle.java

--- Graph/Circle.java
+++ Graph/Circle.java
@@ -1,73 +1,93 @@
 import java.util.*;
 
+//untuk jaringan pertemananannya
 public class Circle {
-    Map<Mahasiswa, Set<Mahasiswa>> graf = new HashMap<>();
+    //graph untuk merepresentasikan mahasiswa dan daftar temannya
+    private Map<String, Set<String>> graph;
 
-    public void addMhs (String name, List<String> friendsList) {
-        Mahasiswa std = new Mahasiswa(name);
-        graf.putIfAbsent(std, new HashSet<>());
+    public Circle() {
+        //inisialisasi graph
+        graph = new HashMap<>();
+    }
 
-        for (String friendName: friendsList) {
-            Mahasiswa friend = new Mahasiswa(friendName);
-            graf.putIfAbsent(friend, new HashSet<>());
-            graf.get(std).add(friend);
-            graf.get(friend).add(std);
+    //methode untuk menambahkan mahasiswa dan temannya
+    public void addStudent (String name, List<String> friends) {
+        //jika nama mahasiswa tidak ada, tambahkan mahasiswa ke graph
+        graph.putIfAbsent (name, new HashSet<>());
+
+        //loop ini untuk menambahkan setiap nama teman ke dlm daftar teman mahasiswa
+        for (String friendName: friends) {
+            //menambahkan teman ke graph, jika tidak ada
+            graph.putIfAbsent (friendName, new HashSet<>());
+            //menambahkan teman ke daftar teman mahasiswa
+            graph.get(name).add(friendName);
+            //menambahkan mahasiswa ke daftar temannya
+            graph.get(friendName).add(name);
         }
     }
 
-    public void addCircle (String name1, String name2) {
-        Mahasiswa m1 = new Mahasiswa(name1);
-        Mahasiswa m2 = new Mahasiswa(name2);
-
-        if (graf.containsKey(m1) && graf.containsKey(m2)) {
-            graf.get(m1).add(m2);
-            graf.get(m2).add(m1);
+    //methode untuk menambahkan hubungan pertemanan dua mahasiswa
+    public boolean addFriendship (String std1, String std2) {
+        //jika salah satu tidak ditemukan, akan mengembalikan nilai false
+        if (!graph.containsKey(std1) || !graph.containsKey(std2)) {
+            return false;
         }
+        
+        //jika ditemukan, maka akan dibuat hubungan pertemanan
+        graph.get(std1).add(std2);
+        graph.get(std2).add(std1);
+        return true;
     }
 
-    public void removeMhs (String name) {
-        Mahasiswa std = new Mahasiswa(name);
-        if (graf.containsKey(std)) {
-            for (Mahasiswa friend: graf.get(std)) {
-                graf.get(friend).remove(std);
-            }
-            graf.remove(std);
+    //methode untuk menghapus mahasiswa dari semua daftar teman
+    public boolean remove (String name) {
+        //jika nama mahasiswa tidak ditemukan
+        if (!graph.containsKey(name)) {
+            return false;
         }
+
+        //jika nama mahasiswa ditemukan 
+        for (String friendName: graph.get(name)) {
+            //menghapus mahasiswa dari daftar temannya
+            graph.get(friendName).remove(name);
+        }
+        //menghapus mahasiswa dari graph
+        graph.remove(name);
+        return true;
     }
 
-    public void search (String name1, String name2) {
-        Mahasiswa m1 = new Mahasiswa(name1);
-        Mahasiswa m2 = new Mahasiswa(name2);
-
-        if (graf.containsKey(m1) && graf.containsKey(m2)) {
-            Set<Mahasiswa> friend1 = graf.get(m1);
-            Set<Mahasiswa> friend2 = graf.get(m2);
-
-            Set<Mahasiswa> find = new HashSet<>(friend1);
-            find.retainAll(friend2);
-
-            if (find.isEmpty()) {
-                System.out.println("No mutual friends");
-            }
-            else {
-                for (Mahasiswa m: find) {
-                    System.out.println("-> " + m.name);
-                }
-            }
+    //methode untuk mencari teman bersama antara dua mahasiswa
+    public Set<String> mutualFriends (String std1, String std2) {
+        //jika salah satu tidak ditemukan, maka mengembalikan nilai null
+        if (!graph.containsKey(std1) || !graph.containsKey(std2)) {
+            return null;
         }
+
+        //jika ditemukan, maka mengambil daftar teman dari masing-masing mahasiswa
+        Set<String> f1 = graph.get(std1);
+        Set<String> f2 = graph.get(std2);
+
+        //untuk membuat salinan dari daftar teman std1 agar tidak mengubah aslinya
+        Set<String> mutual = new HashSet<>(f1);
+        
+        //menyisakan teman yg hanya dimiliki keduanya (irisan antara f1 & f2)
+        mutual.retainAll(f2);
+        return mutual;
     }
 
-    public void display (String name) {
-        Mahasiswa std = new Mahasiswa(name);
+    //methode untuk mendapatkan daftar teman dari mahasiswa
+    public Set<String> getFriends (String name) {
+        //jika tidak ada, maka akan mengembalikan set kosong
+        return graph.getOrDefault (name, Collections.emptySet());
+    }
 
-        if (graf.containsKey(std)) {
-            System.out.println("Friends of " + name + ": ");
-            for (Mahasiswa friend: graf.get(std)) {
-                System.out.println("-> " + friend.name);
-            }
-        }
-        else {
-            System.out.println("Not found");
-        }
+    //methode untuk mengecek apakah mahasiswa ada atau tidak
+    public boolean exist (String name) {
+        return graph.containsKey(name);
+    }
+
+    //methode untuk mendapatkan semua mahasiswa yg terdaftar
+    public Set<String> allStudents() {
+        return graph.keySet();
     }
 }
